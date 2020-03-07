5545a0314f6a2daf6f10a94c2de125d52af46887
Niklas Loeser <niklas@4loeser.net>
2020-03-07 12:57:45 +0100

    adds a html template for the readability command

--- newc.cr
+++ newc.cr
@@ -25,53 +25,129 @@ module RSS
   end
 end
 
-module Newc
-    @@settings_dir : Path = Path["~/.local/share/newc"].expand(home: true)
-    @@settings_file : Path = @@settings_dir / Path["settings.json"]
-    @@cache_file : Path = @@settings_dir / Path["cache.json"]
-    @@settings : JSON::Any = JSON.parse("{}")
+class Newc
 
-    def self.initialize
-        if (!File.exists? @@settings_dir)
-            FileUtils.mkdir_p(@@settings_dir.to_s)
+    @settings_dir : Path
+    @settings_file : Path
+    @cache_file : Path
+    @settings : JSON::Any
+    @@html_template : String = <<-TEMPLATE
+   <!DOCTYPE html>
+   <html>
+   <head>
+     <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
+     <style>
+       :root{
+         --background-color: #FEFEFE;
+         --color: #232323;
+         --accent: #424242;
+         --font: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+       }
+        blockquote{
+          border-inline-start: 4px solid var(--accent);
+          padding-inline-start: 20px;
+        }
+        body{
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          background-color: var(--background-color);
+          color: var(--color);
+
+          font-family: var(--font);
+        }
+        #content{
+          width: 35%;
+        }
+        #title{
+          font-size: 1.8em;
+          line-height: 1.40em;
+          margin-bottom: 0.5em;
+          margin-top: 0.8em;
+        }
+        #page{
+          margin-top: 2em;
+          line-height: 1.5em;
+          font-size: large;
+        }
+        #author{
+          line-height: 2em;
+          max-width: max-content;
+        }
+        .line{
+          width: 100%;
+          height: 1px;
+          background-color: var(--color);
+        }
+    </style>
+   </head>  
+   <body>
+      <div id="content">
+        <div id="author">
+          {{{Author}}}
+          <div class="line"></div>
+        </div>
+        <h1 id="title">
+          {{{Title}}}
+        </h1>
+        <div class="line" ></div>
+        <div id="page">
+          {{{Page}}}  
+        </div>
+      </div>
+   </body>   
+   </html>
+TEMPLATE
+
+
+    def initialize(settings_dir : Path)
+        #@@settings_dir = (settings_dir.as(Path) || Path["~/.local/share/newc"].expand(home: true)).as(Path)
+        @settings_dir = settings_dir
+        @settings_file = @settings_dir / Path["settings.json"]
+        @cache_file = @settings_dir / Path["cache.json"]
+        @settings = JSON.parse("{}")
+
+        if (!File.exists? @settings_dir)
+            FileUtils.mkdir_p(@settings_dir.to_s)
         end
 
-        if (!File.exists? @@settings_file)
-            File.write(@@settings_file.to_s,
+        if (!File.exists? @settings_file)
+            File.write(@settings_file.to_s,
             "{
                 \"expire_time\" : 10,
                 \"last_refresh\" : -1,
                 \"feeds\" : []
             }")
         end
-        if (!File.exists? @@cache_file)
-            File.write(@@cache_file.to_s,"{}")
+        if (!File.exists? @cache_file)
+            File.write(@cache_file.to_s,"{}")
         end
 
-        @@settings = self.load_settings
+        @settings = self.load_settings
+    
     end
     
 
-    def self.load_settings()
-        JSON.parse(File.read(@@settings_file))
+    def load_settings()
+        JSON.parse(File.read(@settings_file))
     end
 
 
-    def self.expired?()
-        diff_seconds = (Time.utc.to_unix - @@settings["last_refresh"].as_i)
-        diff_seconds > @@settings["expire_time"].as_i * 60
+    def expired?()
+        diff_seconds = (Time.utc.to_unix - @settings["last_refresh"].as_i)
+        diff_seconds > @settings["expire_time"].as_i * 60
     end
 
 
-    def self.load_rss(force)
+    def load_rss(force)
         feed_items = [] of RSS::Item
 
         if !force && !self.expired?
-            feed_items = Array(RSS::Item).from_json(File.read(@@cache_file))
+            feed_items = Array(RSS::Item).from_json(File.read(@cache_file))
         else
             channel = Channel(Nil).new
 
-            feeds = @@settings["feeds"].as_a
+            feeds = @settings["feeds"].as_a
             feeds.each do |feed|
                 spawn do
                     rss_feed = RSS.parse feed.to_s
@@ -91,11 +167,11 @@ module Newc
                 unix_a < unix_b ? -1 : (unix_a == unix_b ? 0 : 1)
             end
 
-            File.write(@@cache_file, feed_items.to_json)
+            File.write(@cache_file, feed_items.to_json)
             changed_settings = {
-                "expire_time" => @@settings["expire_time"],
+                "expire_time" => @settings["expire_time"],
                 "last_refresh" => Time.utc.to_unix,
-                "feeds" => @@settings["feeds"]
+                "feeds" => @settings["feeds"]
             }
             self.save_settings(changed_settings)
 
@@ -105,16 +181,15 @@ module Newc
     end
 
 
-    def self.save_settings(some_hash)
-        File.write(@@settings_file, some_hash.to_json)
+    def save_settings(some_hash)
+        File.write(@settings_file, some_hash.to_json)
     end
 
-
-    def self.add_feed(feed)
-        feeds = @@settings["feeds"].as_a
+    def add_feed(feed)
+        feeds = @settings["feeds"].as_a
         feeds << JSON::Any.new(feed)
         changed_settings = {
-            "expire_time" => @@settings["expire_time"],
+            "expire_time" => @settings["expire_time"],
             "last_refresh" => -1,
             "feeds" => feeds
         }
@@ -122,11 +197,11 @@ module Newc
     end
 
 
-    def self.remove_feed(feed)
-        feeds = @@settings["feeds"].as_a
+    def remove_feed(feed)
+        feeds = @settings["feeds"].as_a
         feeds.delete(JSON::Any.new(feed))
         changed_settings = {
-            "expire_time" => @@settings["expire_time"],
+            "expire_time" => @settings["expire_time"],
             "last_refresh" => -1,
             "feeds" => feeds
         }
@@ -134,7 +209,7 @@ module Newc
     end
 
 
-    def self.to_xml(result)
+    def to_xml(result)
         XML.build(indent: "  ") do |xml|
             xml.element("feed") do
 
@@ -155,7 +230,7 @@ module Newc
         end
     end
 
-    def self.to_readable(result)
+    def to_readable(result)
         String.build do |str|
             result.each do |item|
                 str << "\n"
@@ -170,7 +245,7 @@ module Newc
     end
 
 
-    def self.print_feed(format, force)
+    def print_feed(format, force)
         if format == "json"
             puts self.load_rss(force).to_json
 
@@ -186,7 +261,7 @@ module Newc
         end
     end
 
-    def self.print_readable(format, url)
+    def print_readable(format, url)
         response = HTTP::Client.get url
 
         if format == "readable"
@@ -219,18 +294,20 @@ module Newc
                 end
             end
             puts xml
+        elsif format == "page"
+            puts @@html_template.gsub("{{{Author}}}", res["author"]).gsub("{{{Title}}}", res["title"]).gsub("{{{Page}}}", res["content"])
         elsif format == "readable"
             puts res["title"], res["author"], "----------------------------", res["content"].split(/\.(?=\S{3,})/).join(".\n\n")
         end
     end
 
-    def self.print_settings()
-        puts @@settings.to_json
+    def print_settings()
+        puts @settings.to_json
     end
 
-    def self.print_setting(setting)
-        if @@settings.as_h.has_key?(setting)
-            puts @@settings[setting]
+    def print_setting(setting)
+        if @settings.as_h.has_key?(setting)
+            puts @settings[setting]
         else
             puts "Settings does not exist"
         end
@@ -238,18 +315,12 @@ module Newc
 end
 
 
-#document = Readability::Document.new(response.body)
-#puts document.content
-#puts document.meta_image
-
-Newc.initialize
-
-#Time.utc.to_unix --> Number of seconds
-
 TAB = "    "
 
 output_format = "json"
+settings_dir = Path["~/.local/share/newc"].expand(home: true)
 force = false
+newc : Newc
 
 OptionParser.parse do |parser|
 
@@ -259,6 +330,10 @@ OptionParser.parse do |parser|
         puts "#{TAB}readability\t\t\tPrints a more readable version of a website to the console in a given output format","","#{TAB}#{TAB}Arguments:","#{TAB}#{TAB}#{TAB}1) url of the website","#{TAB}#{TAB}Default output format: json", "#{TAB}#{TAB}Example: newc readability https://www.testwebsite.com/","",""
         puts "#{TAB}list\t\t\tLists settings","","#{TAB}#{TAB}Arguments:","#{TAB}#{TAB}#{TAB}1) the name of the setting","#{TAB}#{TAB}If setting name is omitted, all settings are shown", "#{TAB}#{TAB}Example: newc list feeds","",""
     }
+
+    parser.on "-c", "--config=DIR", "Config directory" do |dir|
+        settings_dir = Path[dir].expand(home: true).normalize()
+    end
 
 
     parser.on "-h", "--help", "Shows the help" do
@@ -274,6 +349,10 @@ OptionParser.parse do |parser|
         output_format = "xml"
     end
 
+    parser.on "-p", "--page", "Page (HTML) output format" do
+        output_format = "page"
+    end
+
     parser.on "-r", "--readable", "Readable output format" do
         output_format = "readable"
     end
@@ -283,13 +362,14 @@ OptionParser.parse do |parser|
     end
 
     parser.unknown_args do |args_arr|
+        newc = Newc.new(settings_dir)
         if args_arr.size > 0
             case args_arr[0]
                 when "feed"
-                    Newc.print_feed(output_format, force)
+                    newc.print_feed(output_format, force)
                 when "readability"
                     if args_arr.size == 2
-                        Newc.print_readable(output_format, args_arr[1])
+                        newc.print_readable(output_format, args_arr[1])
                     else
                         puts "Needs one URL as a second parameter"
                         show_help.call
@@ -297,16 +377,16 @@ OptionParser.parse do |parser|
                     end
                 when "list"
                     if args_arr.size == 1
-                        Newc.print_settings()
+                        newc.print_settings()
                     else
-                        Newc.print_setting(args_arr[1])
+                        newc.print_setting(args_arr[1])
                     end
                 when "add"
                     if args_arr.size != 2
                         puts "You must provide an url to a rss feed"
                         show_help.call
                     else
-                        Newc.add_feed(args_arr[1])
+                        newc.add_feed(args_arr[1])
                     end
 
                 when "remove"
@@ -314,10 +394,10 @@ OptionParser.parse do |parser|
                         puts "You must provide an url to a rss feed"
                         show_help.call
                     else
-                        Newc.remove_feed(args_arr[1])
+                        newc.remove_feed(args_arr[1])
                     end
                 else
-                    Newc.print_feed(output_format, force)
+                    newc.print_feed(output_format, force)
 
             end
         else
@@ -327,4 +407,3 @@ OptionParser.parse do |parser|
         end
     end
 end
-
