1d054d86082c9c2a3a712094a97cd7d447667d3a
Niklas Loeser <niklas@4loeser.net>
2020-03-16 21:46:10 +0100

    added comments

--- newc.cr
+++ newc.cr
@@ -43,6 +43,10 @@ class Newc
          --accent: {{{Accent}}};
          --font: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
        }
+       img{
+           max-width: 100%;
+           height: auto;
+       }
         blockquote{
           border-inline-start: 4px solid var(--accent);
           padding-inline-start: 20px;
@@ -142,11 +146,14 @@ TEMPLATE
     def load_rss(force)
         feed_items = [] of RSS::Item
 
+        # Not expired?
         if !force && !self.expired?
+            # Loaded from cache
             feed_items = Array(RSS::Item).from_json(File.read(@cache_file))
         else
             channel = Channel(Nil).new
 
+            # Gets the RSS feeds in parallel
             feeds = @settings["feeds"].as_a
             feeds.each do |feed|
                 spawn do
@@ -156,17 +163,19 @@ TEMPLATE
                     channel.send(nil)
                 end
             end
+            # Waits for all feeds to be done
             feeds.size.times do 
                 channel.receive
             end
 
-
+            # RSS items sorted by date
             feed_items.sort do |a,b|
                 unix_a = Time.parse(a.pubDate.gsub("GMT","+0000"),"%a, %d %b %Y %T %z", Time::Location::UTC).to_unix
                 unix_b = Time.parse(b.pubDate.gsub("GMT","+0000"),"%a, %d %b %Y %T %z", Time::Location::UTC).to_unix
                 unix_a < unix_b ? -1 : (unix_a == unix_b ? 0 : 1)
             end
 
+            # Saved to cache
             File.write(@cache_file, feed_items.to_json)
             changed_settings = {
                 "expire_time" => @settings["expire_time"],
@@ -185,6 +194,7 @@ TEMPLATE
         File.write(@settings_file, some_hash.to_json)
     end
 
+    # Adds a rss feed to the settings
     def add_feed(feed)
         feeds = @settings["feeds"].as_a
         feeds << JSON::Any.new(feed)
@@ -197,6 +207,7 @@ TEMPLATE
     end
 
 
+    # Delete a rss feed from the settings
     def remove_feed(feed)
         feeds = @settings["feeds"].as_a
         feeds.delete(JSON::Any.new(feed))
@@ -245,22 +256,28 @@ TEMPLATE
     end
 
 
+    # Print a rss feed
+    # Force: if all of the feeds should be reloaded or taken from the cache (if they did not expire)
     def print_feed(format, force)
+        
+        result = self.load_rss(force)
         if format == "json"
-            puts self.load_rss(force).to_json
+            puts result.to_json
 
         elsif format == "xml"
-            result = self.load_rss(force)
             xml = self.to_xml(result)
             puts xml
 
         elsif format == "readable"
-            result = self.load_rss(force)
             readable = self.to_readable(result)
             puts readable
         end
     end
 
+    # Takes an url and returns a more readable version of the webpage
+    # Either in xml format, json format, or as a html page (or readable to the command line)
+    # HTML theme (light, dark, sepia) can also be set
+    # Uses the readability library
     def print_readable(format, url, theme)
         response = HTTP::Client.get url
 
@@ -277,6 +294,8 @@ TEMPLATE
         end
         document = Readability::Document.new(response.body,options)
     
+        # Sets all of the important information
+        # Title, author and content of the page / article
         res = {
             "title" => document.title || "",
             "author" => document.author || "",
@@ -286,6 +305,7 @@ TEMPLATE
         if format == "json"
             puts res.to_json
         elsif format == "xml"
+            # XML Builder
             xml = XML.build(indent: "  ") do |xml|
                 xml.element("article") do
                     xml.element("title"){xml.text res["title"]}
@@ -295,6 +315,7 @@ TEMPLATE
             end
             puts xml
         elsif format == "page"
+            # Fill the html template
             filled_template = @@html_template.gsub("{{{Author}}}", res["author"]).gsub("{{{Title}}}", res["title"]).gsub("{{{Page}}}", res["content"])
             case theme
                 when "light"
@@ -315,14 +336,17 @@ TEMPLATE
             end
             puts filled_template.gsub("{{{BackgroundColor}}}", background_color).gsub("{{{Color}}}", color).gsub("{{{Accent}}}", accent)
         elsif format == "readable"
+            # Just print to command line
             puts res["title"], res["author"], "----------------------------", res["content"].split(/\.(?=\S{3,})/).join(".\n\n")
         end
     end
 
+    # Print all settings
     def print_settings()
         puts @settings.to_json
     end
 
+    # Print a specific setting to the command line
     def print_setting(setting)
         if @settings.as_h.has_key?(setting)
             puts @settings[setting]
@@ -342,12 +366,14 @@ newc : Newc
 page_theme = ""
 
 OptionParser.parse do |parser|
-
+    # Help to be shown
     show_help = ->{
         puts parser,"",""
         puts "Commands:","","#{TAB}feed\t\t\tPrints the news feed to the console in a given output format","","#{TAB}#{TAB}Default output format: json","#{TAB}#{TAB}Example: newc -x feed","",""
         puts "#{TAB}readability\t\t\tPrints a more readable version of a website to the console in a given output format","","#{TAB}#{TAB}Arguments:","#{TAB}#{TAB}#{TAB}1) url of the website","#{TAB}#{TAB}Default output format: json", "#{TAB}#{TAB}Example: newc readability https://www.testwebsite.com/","",""
         puts "#{TAB}list\t\t\tLists settings","","#{TAB}#{TAB}Arguments:","#{TAB}#{TAB}#{TAB}1) the name of the setting","#{TAB}#{TAB}If setting name is omitted, all settings are shown", "#{TAB}#{TAB}Example: newc list feeds","",""
+        puts "#{TAB}add\t\t\tAdd feed","","#{TAB}#{TAB}Arguments:","#{TAB}#{TAB}#{TAB}1) the url of the feed","#{TAB}#{TAB}Example: newc add http://feeds.bbci.co.uk/news/technology/rss.xml","",""
+        puts "#{TAB}remove\t\t\Remove feed","","#{TAB}#{TAB}Arguments:","#{TAB}#{TAB}#{TAB}1) the url of the feed","#{TAB}#{TAB}Example: newc remove http://feeds.bbci.co.uk/news/technology/rss.xml","",""
     }
 
     parser.on "-c", "--config=DIR", "Config directory" do |dir|
